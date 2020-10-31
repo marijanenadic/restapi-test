aa1ce15361e118f51faf4c44a5f9a43843cc04e0
marijanenadic <43967653+marijanenadic@users.noreply.github.com>
2020-10-31 21:44:15 +0100

    Domaci

--- obrada.php
+++ obrada.php
@@ -23,5 +23,50 @@
             $_POST = array();
             exit();
         }
+        //DOMACI ZADATAK
+        //Insert nove kategorije
+        elseif($_POST["kategorija_naziv"] != null) {
+            $naziv = $_POST["kategorija_naziv"];
+            if ($mydb->insert("kategorije", "naziv", [$naziv])) {
+                echo "kategorija ubacena";
+            } else {
+                echo "kategorija nije ubacena";
+            }
+            $_POST = array();
+            exit();
+        }
+        //Update kategorije
+        elseif($_POST["kategorija_id"] != null && $_POST["kategorija_naziv_put"] != null) {
+            $naziv = $_POST["kategorija_naziv_put"];
+            if ($mydb->update("kategorije", $_POST["kategorija_id"], "naziv", [$naziv])) {
+                echo "kategorija je izmjenjena";
+            } else {
+                echo "kategorija nije izmjenjena";
+            }
+            $_POST = array();
+            exit();
+        }
+        //Update novosti
+        elseif($_POST["novosti_id"] != null && $_POST["naslov_novosti_put"] != null && $_POST["tekst_novosti_put"] != null && $_POST["kategorija_odabir_put"] != null) {
+        $niz = ["naslov"=> "'".$_POST["naslov_novosti_put"]."'", "tekst"=>"'".$_POST["tekst_novosti_put"]."'", "datumvreme"=>"NOW()", "kategorija_id"=>$_POST["kategorija_odabir_put"]];
+        if ($mydb->update("novosti", "novosti_id", "naslov, tekst, datumvreme, kategorija_id", $niz)) {
+            echo "novost je izmjenjena";
+        } else {
+            echo "novost nije izmjenjena";
+        }
+        $_POST = array();
+        exit();
+        }
+        //Prikaz vrednosti iz tabele, nebitno da li je odabrana kategorija ili novost
+        else {
+        $tabela = $_POST["odabir_tabele"];
+        if ($mydb->select($tabela, "*", null, null, null)) {
+            echo "tabela je prikazana";
+        } else {
+            echo "tabela nije prikazana";
+        }
+        $_POST = array();
+        exit();
+        }
     }
 ?>
